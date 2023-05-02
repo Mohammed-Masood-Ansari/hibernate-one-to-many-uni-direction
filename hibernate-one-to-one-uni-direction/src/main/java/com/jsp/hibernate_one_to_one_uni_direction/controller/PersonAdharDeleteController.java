d90c4aab77610472dad6960855ba1eff8e908dbd
Mohammed-Masood-Ansari <masoodansari1997@gmail.com>
2023-05-02 17:47:47 +0530

    updated one to one

--- hibernate-one-to-one-uni-direction/src/main/java/com/jsp/hibernate_one_to_one_uni_direction/controller/PersonAdharDeleteController.java
+++ hibernate-one-to-one-uni-direction/src/main/java/com/jsp/hibernate_one_to_one_uni_direction/controller/PersonAdharDeleteController.java
@@ -10,6 +10,9 @@ import com.jsp.hibernate_one_to_one_uni_direction.service.PersonService;
  */
 public class PersonAdharDeleteController {
 
+	/*
+	 * main method
+	 */
 	public static void main(String[] args) throws IdNotFoundException {
 		
 		PersonService personService = new PersonService();
